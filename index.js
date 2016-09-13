ac343543173165ba7a2316e2d7e9a635ebf979a4
Dan Andrei <andreidani96@gmail.com>
2016-09-13 14:41:34 +0300

    Module structure.

---
+++ index.js
@@ -0,0 +1,14 @@
+// Dependencies
+
+/* public function */
+
+/*
+ *
+ * initilizes the remodal module
+ * @name init
+ *
+ */
+exports.init = function (config, ready) {
+	this._config = config;
+	ready();
+}
